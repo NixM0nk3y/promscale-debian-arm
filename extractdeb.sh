5bf40c49ca49dd88ecdd61707412f48e131272ca
Nick Gregory <github@openenterprise.co.uk>
2021-05-23 18:43:42 +0000

    correct docker repo

--- extractdeb.sh
+++ extractdeb.sh
@@ -2,7 +2,7 @@
 
 DIRNAME=$(dirname "$0")
 
-containerID=$(docker run --detach nixm0nk3y/timescale-prometheus-debian-arm:latest /bin/sleep 120)
+containerID=$(docker run --detach docker.io/nixm0nk3y/promscale-debian-arm:latest /bin/sleep 120)
 docker exec $containerID bash -c "mkdir /tmp/packages; mv /tmp/*.deb /tmp/packages"
 docker cp "$containerID:/tmp/packages" .
 sleep 1
